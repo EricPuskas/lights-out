845067572e18cd1da4962a84f442ed2d44e4bbbd
unknown <eric.puskas@yahoo.com>
2019-07-31 12:38:39 +0300

    Updates

---
+++ precache-manifest.acb2b686353217094af9cd425ccb2dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdaf10c80ca5dc5340a0c4a58b3fef8",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "20ef19b0313ce00db60b",
+    "url": "/lights-out/static/css/main.32d1fa17.chunk.css"
+  },
+  {
+    "revision": "dbd9b9d1d09f56a8fe13",
+    "url": "/lights-out/static/js/2.63a3d80a.chunk.js"
+  },
+  {
+    "revision": "20ef19b0313ce00db60b",
+    "url": "/lights-out/static/js/main.70799d37.chunk.js"
+  },
+  {
+    "revision": "d6b7daafe64bb82a9237",
+    "url": "/lights-out/static/js/runtime~main.69e4817a.js"
+  }
+]);
